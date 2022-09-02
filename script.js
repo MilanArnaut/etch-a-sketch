597db2e2d034a977702b464da6374e867f9a43de
MilanArnaut <milanarnaut02@gmail.com>
2022-09-02 22:56:41 +0200

    Added functionality for erase button

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
-let range = document.querySelector('#range');
-let rangeNumber = document.querySelector('#rangeNumber'); 
-let container = document.querySelector('.main-section .grid-container');
+const range = document.querySelector('#range');
+const rangeNumber = document.querySelector('#rangeNumber'); 
+const container = document.querySelector('.main-section .grid-container');
+const eraseButton = document.querySelector('#eraseButton');
+const rainbowButton = document.querySelector('#rainbowButton');
+const clearButton = document.querySelector('#clearButton');
 let size = 16;
 
-range.addEventListener('change', e => {
-    e.preventDefault();
-    rangeNumber.innerHTML = `${range.value}x${range.value}`;
-    size = parseInt(range.value);
-    container.setAttribute('style', `grid-template-columns: repeat(${size}, auto);`);
-    console.log(range.value);
-    container.innerHTML = '';
-    for(let i = 1; i <= size*size; i++) {
-        let div = document.createElement('div');
-        div.classList.add('grid-item');
-        div.addEventListener('mouseenter', e => {
-            e.preventDefault();
-
-            let colorPicker = document.querySelector('#colorPicker').value;
-            div.style.backgroundColor = `${colorPicker}`;
-        })
-        container.appendChild(div);
-    }
-})
-
+//First load to make grid items
 for(let i = 1; i <= size*size; i++) {
     let div = document.createElement('div');
     div.classList.add('grid-item');
@@ -39,3 +23,35 @@ gridItems.forEach(gridItem => {
         gridItem.style.backgroundColor = `${colorPicker}`;
     });
 });
+
+//changing grid items
+range.addEventListener('change', e => {
+    e.preventDefault();
+    rangeNumber.innerHTML = `${range.value}x${range.value}`;
+    size = parseInt(range.value);
+    container.setAttribute('style', `grid-template-columns: repeat(${size}, auto);`);
+    container.innerHTML = '';
+    for(let i = 1; i <= size*size; i++) {
+        let div = document.createElement('div');
+        div.classList.add('grid-item');
+        div.addEventListener('mouseenter', e => {
+            e.preventDefault();
+
+            let colorPicker = document.querySelector('#colorPicker').value;
+            div.style.backgroundColor = `${colorPicker}`;
+        })
+        container.appendChild(div);
+    }
+})
+
+//clearing grid
+clearButton.addEventListener('click', e => {
+    e.preventDefault();
+    let gridItems = document.querySelectorAll('.main-section .grid-container .grid-item');
+    gridItems.forEach(item => {
+        item.style.backgroundColor = 'white';
+    });
+});
+
+
+console.log(document.getElementsByClassName('main-section.grid-container'));
